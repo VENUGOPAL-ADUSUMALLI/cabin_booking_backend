d909de31f3ca536f8bcda884cfe5406697ccfd8f
venu <adusumallivenugopal04@gmail.com>
2024-11-14 10:57:51 +0530

    changes in wsgi

--- cabin_booking_backend/wsgi.py
+++ cabin_booking_backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cabin_booking_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cabin_booking_backend.beta_settings')
 
 application = get_wsgi_application()
